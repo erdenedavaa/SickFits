c306854682b5e576093cc5446c8210e5050a1637
erdenedavaa <erdenedavaa@users.noreply.github.com>
2021-06-21 14:24:43 +0800

    Lesson 9

---
+++ pages/index.js
@@ -0,0 +1,3 @@
+export default function IndexPage() {
+  return <p>This is index page</p>;
+}
---
+++ pages/sale.js
@@ -0,0 +1,7 @@
+export default function SalePage() {
+  return (
+    <div>
+      <p>This is sale page</p>
+    </div>
+  );
+}
